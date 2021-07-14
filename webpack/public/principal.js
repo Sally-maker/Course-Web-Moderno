c1155bbc5a7c39dc0de04592d00a28b43bfa6b0d
Sally-maker <gabrielgbbarbosa624@gmail.com>
2021-07-14 10:00:07 -0300

    Web Moderno

---
+++ webpack/public/principal.js
@@ -0,0 +1,1 @@
+(()=>{var e={284:(o,r,e)=>{const s=e(770);console.log(s.saudacao())},770:o=>{o.exports={saudacao(){return"Olá eu sou o Módulo B!!!"}}}},s={};function t(o){var r=s[o];if(void 0!==r)return r.exports;r=s[o]={exports:{}};return e[o](r,r.exports,t),r.exports}(()=>{"use strict";t(284);const o=new class{cumprimentar(){return"Bom dia!"}};console.log(o.cumprimentar())})()})();
